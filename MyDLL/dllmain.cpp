ad6e0d197149c315c11d1a139ff78643ff79b280
alexbelan1999 <alexbelan1999@gmail.com>
2020-04-29 15:49:43 +0300

    Add MyDLL project

---
+++ MyDLL/dllmain.cpp
@@ -0,0 +1,30 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+extern "C" __declspec(dllexport) char Message[] = "Hello World!";
+extern "C" __declspec(dllexport) void SayMessage(char* message) 
+{
+	cout << "DLL say> " << message << endl;
+	return;
+}
+
+BOOL WINAPI DLLMain(
+	HINSTANCE hinstDll,
+	DWORD fdwReason,
+	LPVOID lpvReserved
+) {
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+
+}
