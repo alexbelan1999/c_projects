992a621077e65f3cdf95762eb38a11d48499e853
alexbelan1999 <alexbelan1999@gmail.com>
2020-04-29 16:07:02 +0300

    Add DLL_RUS project

---
+++ DLL_RUS/dllmain.cpp
@@ -0,0 +1,40 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "stdafx.h"
+#include "resource.h"
+#include <iostream>
+#include <Windows.h>
+
+__declspec(dllexport) HICON hIcon1;
+
+
+using namespace std;
+
+extern "C" __declspec(dllexport) LPCSTR text = "Введите текст";
+extern "C" __declspec(dllexport) LPCSTR substr = "Введите подстроку";
+extern "C" __declspec(dllexport) LPCSTR calc = "Подсчитать";
+extern "C" __declspec(dllexport) LPCSTR result = "Результат:";
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH: {
+		hIcon1 = LoadIcon(hModule, MAKEINTRESOURCE(IDI_ICON1));
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH: {
+		DestroyIcon(hIcon1);
+		break;
+	}
+	}
+	return TRUE;
+}
+
+
+
+
